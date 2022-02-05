d3cf72e3501478ad8732c8f1e7c3b4073ee83a69
lunseil <lunseil@qq.com>
2022-02-06 00:24:04 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# AutoBuild-LEDE
+
+[![LICENSE](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat&logo=github&label=LICENSE)](https://github.com/lunseil/AutoBuild-LEDE/blob/main/LICENSE)
+![GitHub Stars](https://img.shields.io/github/stars/lunseil/AutoBuild-LEDE.svg?style=flat&logo=appveyor&label=Stars&logo=github)
+![GitHub Forks](https://img.shields.io/github/forks/lunseil/AutoBuild-LEDE.svg?style=flat&logo=appveyor&label=Forks&logo=github)
+![GitHub last commit](https://img.shields.io/github/last-commit/lunseil/AutoBuild-LEDE?label=Latest%20Commit&logo=github)
+
+使用GitHub Actions构建OpenWrt固件精益的OpenWrt，
+特此感谢P3TERX的出色工作：https://github.com/P3TERX/Actions-OpenWrt/
+
+在此，感谢KFERMercer所做的出色工作：https://github.com/KFERMercer/OpenWrt-CI
+
+您可以编辑并启用“同步代码” YAML文件，以使您的分叉存储库保持更新。
+
+特此感谢P3TERX的出色工作：https://github.com/esirplayground/AutoBuild-OpenWrt
---
+++ customize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: Lunseil
+#=================================================
+#1. 修改默认的IP以
+sed -i 's/192.168.1.1/10.10.10.222/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 修改主机名
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Soft_Routes'' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. 设置密码为空
+#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#4. 版本号里显示一个自己的名字
+#sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#5. 修改WAN口为PPPOE
+#sed -i 's/username/5161851ST@MYADSL/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/password/？？？？？？/g' openwrt/package/base-files/files/bin/config_generate
