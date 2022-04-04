e90af12c7596a9b1370c999a7f6743d9c02cdd08
lunseil <lunseil@qq.com>
2022-04-04 20:09:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,5 @@
 # Author: Lunseil
 #=================================================
 #1. 修改默认的IP以
-sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.10/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/eth1/eth3/g' openwrt/package/base-files/files/bin/config_generate
